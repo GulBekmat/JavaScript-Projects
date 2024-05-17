29f5399f73f86587685a2a98ff06ee65116bbe29
gigibekmatova <gigibekmatova@mail.ru>
2024-05-16 17:48:35 -0700

    new project

--- tip-calculator/tip-calculator.js
+++ tip-calculator/tip-calculator.js
@@ -1,3 +1,26 @@
 const form = document.querySelector('form');
 
-console.log(form);
+form.addEventListener('submit', function() {
+ event.preventDefault();
+ //getting form controls
+ const billInput = this.elements[0];
+ const serviceInput = this.elements[1];
+ const howManyPeapleInput = this.elements[2];
+ //getting form controls values and parsing to integers, so Math function wuld work.
+ const billInputValue = parseInt(billInput.value);
+ const serviceInputValue = parseInt(serviceInput.value);
+ const howManyPeapleInputValue = parseInt(howManyPeapleInput.value);
+ let  tipFromEach = billInputValue * (serviceInputValue / 100) / howManyPeapleInputValue;
+ tipFromEach = tipFromEach.toFixed(2);
+ const output = `
+ <span>TIP AMOUNT</span>
+ <br>
+ <span>$</span><span>${tipFromEach}</span>
+ <br>
+ <span>each</span>
+ `;
+ const placeholder = document.querySelector("#calculatePlaceholder");
+ placeholder.innerHTML = output;
+ 
+ console.log(billInputValue, serviceInputValue, howManyPeapleInputValue, tipFromEach);
+})
--- word-count/word-count.js
+++ word-count/word-count.js
@@ -6,6 +6,6 @@ const calculate = function(){
  const input = document.querySelector('input');
  const value = input.value;
  const placeholderP = document.getElementById('word-count-placeholder');
- placeholderP.innerText = value.length;
+ placeholderP.innerText = value.split(' ').length;
 }
 button.addEventListener('click', calculate);
